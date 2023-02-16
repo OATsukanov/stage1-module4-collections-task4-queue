5ed3c3fc676f6334ddf1ed10aff1818a03578a4c
oleg <tsukanov.regula@gmail.com>
2023-02-16 22:45:58 +0500

    first try

--- src/main/java/com/epam/collections/queue/ArrayDequeCreator.java
+++ src/main/java/com/epam/collections/queue/ArrayDequeCreator.java
@@ -1,10 +1,34 @@
 package com.epam.collections.queue;
 
 import java.util.ArrayDeque;
+import java.util.Objects;
 import java.util.PriorityQueue;
 import java.util.Queue;
 
 public class ArrayDequeCreator extends PriorityQueue<String> {
     public ArrayDeque<Integer> createArrayDeque(Queue<Integer> firstQueue, Queue<Integer> secondQueue) {
+
+        Queue<Integer> firstArrayDeque = new ArrayDeque<>(firstQueue);
+        Queue<Integer> secondArrayDeque = new ArrayDeque<>(secondQueue);
+
+        ArrayDeque<Integer> thirdArrayDeque = new ArrayDeque<>();
+
+        thirdArrayDeque.addLast(firstArrayDeque.poll());
+        thirdArrayDeque.addLast(firstArrayDeque.poll());
+        thirdArrayDeque.addLast(secondArrayDeque.poll());
+        thirdArrayDeque.addLast(secondArrayDeque.poll());
+
+        while (!firstArrayDeque.isEmpty() || !secondArrayDeque.isEmpty()) {
+
+            firstArrayDeque.add(thirdArrayDeque.pollLast());
+            thirdArrayDeque.offerLast(firstArrayDeque.poll());
+            thirdArrayDeque.offerLast(firstArrayDeque.poll());
+
+            secondArrayDeque.add(thirdArrayDeque.pollLast());
+            thirdArrayDeque.offerLast(secondArrayDeque.poll());
+            thirdArrayDeque.offerLast(secondArrayDeque.poll());
+        }
+
+        return thirdArrayDeque;
     }
 }
--- src/main/java/com/epam/collections/queue/DishOrderDeterminer.java
+++ src/main/java/com/epam/collections/queue/DishOrderDeterminer.java
@@ -1,8 +1,43 @@
 package com.epam.collections.queue;
 
-import java.util.List;
+import java.util.*;
 
 public class DishOrderDeterminer {
     public List<Integer> determineDishOrder(int numberOfDishes, int everyDishNumberToEat) {
+
+    List<Integer> dishList = new LinkedList<>();
+    List<Integer> dishOrder = new ArrayList<>();
+
+    for (int i = 1; i <= numberOfDishes; i++) {
+
+        dishList.add(numberOfDishes, i);
     }
+
+    while (dishList.size() > 0) {
+
+        for (int element : dishList) {
+
+            if (dishList.size() < everyDishNumberToEat) {
+
+                if (dishList.indexOf(element) % everyDishNumberToEat == 0) {
+
+                    dishOrder.add(numberOfDishes, element);
+                    dishList.remove(element);
+                }
+            } else {
+
+                dishOrder.add(numberOfDishes, element);
+                dishList.remove(element);
+            }
+        }
+    }
+
+        return dishOrder;
+    }
+
+
+
 }
+
+
+
